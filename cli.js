e27e55b071b3cc53a17f813e892188c409acada9
JohnC0de <joaocarlos.m014@gmail.com>
2024-09-22 23:44:29 -0300

    feat: improve cli, add chalk

--- cli.js
+++ cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 const degit = require('degit')
-const path = require('path')
-const fs = require('fs')
 const { execSync } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk') // Importando o chalk
 
-const repo = 'usuario/repositorio-template' // substitua pelo seu repositório
+const repo = 'JohnC0de/tsstart'
 
 const emitter = degit(repo, {
   cache: false,
@@ -18,11 +19,28 @@ const dest = process.argv[2] || '.'
 emitter
   .clone(dest)
   .then(() => {
-    console.log('Template clonado com sucesso!')
+    console.log(chalk.green.bold('\n --- ✅ Project cloned successfully! --- '))
 
-    // Opcional: Instalar dependências automaticamente
-    // execSync('npm install', { stdio: 'inherit', cwd: dest });
+    console.log(chalk.blue.bold('\n --- 📦 Installing dependencies... --- '))
+    execSync('bun i', { stdio: 'inherit', cwd: dest })
+
+    console.log(chalk.blue.bold('\n --- 🔧 Setting up environment variables... --- '))
+    const envExamplePath = path.join(dest, '.env.example')
+    const envPath = path.join(dest, '.env')
+    try {
+      fs.copyFileSync(envExamplePath, envPath)
+      console.log(chalk.green('\n --- .env file created successfully. --- '))
+    } catch (err) {
+      console.error(chalk.red('\n --- Error copying .env file:', err))
+    }
+
+    // Configurar o banco de dados
+    console.log(chalk.blue.bold('\n --- 🗄️  Creating DB tables... --- '))
+    execSync('bun push', { stdio: 'inherit', cwd: dest })
+
+    console.log(chalk.green.bold('\n --- 🎉 All done! --- '))
+    console.log(chalk.yellow("\n --- ⚠️  Don't forget to set up the environment variables for OAuth. --- \n"))
   })
   .catch(err => {
-    console.error(err)
+    console.error(chalk.red('\n --- Error cloning project:', err))
   })
